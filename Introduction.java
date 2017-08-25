2c6759d49acf33a82a2e297d25e2647464a30a03
Thofe <bmitrick@gmail.com>
2017-08-25 09:50:36 -0400

    Initial sharing of project

---
+++ Introduction.java
@@ -0,0 +1,28 @@
+/**
+ * This program tells a few things about me.
+ * 
+ * @author Jon Cooper
+ * @version August 23, 2017
+ */
+
+public class Introduction
+{
+    public static void main(String[] args)
+   {
+       // here are some facts about me
+       System.out.println("My name is Jon Cooper")
+       System.out.println("I was born in Chicago.");
+       System.out.println("I went to clemson.");
+       System.out.println("My daughter, Ashlyn, is 16.");
+       System.out.println("I like to camp and hike.");
+       
+       // now store some numbers
+       int year = 2017;
+       int age = 25;
+
+       // What happens here?
+       System.out.println("year - age");
+       System.out.println(year - age);
+       System.out.println("I am "+ age + " years old.");
+    }
+}
